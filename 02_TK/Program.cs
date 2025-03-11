4484c23892214f477947f76cb71259d350e3a4ad
valeriiia2003 <leraspasibukhova@ukr.net>
2025-03-11 14:06:26 +0000

    New changes to a project

---
+++ 02_TK/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _02_TK
+{
+    internal class Program
+    {
+        // 2.	Create a Store<T> class that handles inventory for different products.
+        // Use inheritance to derive Product classes (e.g., Electronics, Clothing)
+        // and implement a method to find the most expensive product using LINQ.
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
